192fa8cddf632be9a116b298a064c80f28412572
patrick-rush <patrick.a.rush@gmail.com>
2020-12-20 11:32:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+function mapToNegativize(array) {
+    return array.map(x => x * -1)
+}
+
+function mapToNoChange(array) {
+    return array.map(x => x)
+}
+
+function mapToDouble(array) {
+    return array.map(x => x * 2)
+}
+
+function mapToSquare(array) {
+    return array.map(x => x * x)
+}
+
+function reduceToTotal(array, start=0) {
+    return array.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    }, start)
+}
+
+function reduceToAllTrue(array) {
+    return array.reduce((accumulator, value) => {
+        return accumulator === !!value
+    }, true)
+}
+
+function reduceToAnyTrue(array) {
+    return array.reduce((accumulator, value) => {
+        return (accumulator === !!value) ? false : true;
+    }, false)
+}
